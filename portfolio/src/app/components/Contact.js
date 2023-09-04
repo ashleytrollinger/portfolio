a4e90a888862748d8b438c930a6c13112090c145
ashleytrollinger <ashley@trollingers.com>
2023-09-04 17:00:51 -0400

    MVP

--- portfolio/src/app/components/Contact.js
+++ portfolio/src/app/components/Contact.js
@@ -5,6 +5,7 @@ export default function Contact() {
     return (
         <section className='contact-section'>
             <h2>Contact</h2>
+            <p>Please reach out if you have any feedback, constructive critism <br></br>(as long as it wont make me cry), or if you want to work together! Job offers/ interest in <br></br>employment are also always welcome!</p>
             <form
                 id="contactform"
                 action="https://formsubmit.io/send/ashley@trollingers.com"
