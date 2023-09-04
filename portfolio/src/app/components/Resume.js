745ede2dd13a7d4ef49027183ffd9e086e9ff317
ashleytrollinger <ashley@trollingers.com>
2023-09-03 21:32:30 -0400

    Resume component created and styled as far as formatting goes. The responsiveness needs to be readdressed but on web browser it looks nice

--- portfolio/src/app/components/Resume.js
+++ portfolio/src/app/components/Resume.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import './Resume.css';
 
-export default function Resume(){
+export default function Resume() {
     return (
-        <p>Resume</p>
+        <section className='resume-section'>
+            <div>
+                <h2>My Resume</h2>
+            </div>
+            <div className='link-to'>
+                <a href={'../images/resume.pdf'} download>
+                    <h3>Download my Resume</h3>
+                </a>
+            </div>
+            <div className='skills'>
+                <div className='column-one'>
+                    <h5>Front-End Proficiencies</h5>
+                    <ol>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>Bootstrap</li>
+                        <li>React</li>
+                        <li>version control/Git</li>
+                        <li>Responsive Design</li>
+                    </ol>
+                    <br></br>
+                    <h5>Dev Tool Proficiencies</h5>
+                    <ol>
+                        <li>Git</li>
+                        <li>npm</li>
+                    </ol>
+
+                </div>
+                <div className='column-two'>
+                    <h5>Back-End Proficiencies</h5>
+                    <ol>
+                        <li>APIs</li>
+                        <li>NodeJS</li>
+                        <li>Express</li>
+                        <li>Model View Controller (MVC)</li>
+                        <li>Progressive Web Applications (PWA)</li>
+                    </ol>
+                    <br></br>
+                    <h5>Database Proficiencies</h5>
+                    <ol>
+                        <li>MySQL</li>
+                        <li>Sequelize</li>
+                        <li>NoSQL</li>
+                        <li>SQLite</li>
+                        <li>MongoDB</li>
+                    </ol>
+                </div>
+            </div>
+        </section>
     )
 }
