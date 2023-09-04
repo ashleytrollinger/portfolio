2af064b7c7f0e71bb28fca97937eca81efa0b7b3
ashleytrollinger <ashley@trollingers.com>
2023-09-04 11:07:01 -0400

    Projects have been added and formatted. Now time to polish UI and make sure everything is fully responsive

---
+++ portfolio/src/app/builders/ProjectCards.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ProjectCards.css';
+
+export default function ProjectCard({ title, description, imageUrl, liveLink, githubLink }) {
+
+    return (
+        <div className="project-card">
+            <img src={imageUrl} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                Live Link
+            </a>
+            <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                GitHub Link
+            </a>
+        </div>
+    );
+};
+
--- portfolio/src/app/components/Projects.js
+++ portfolio/src/app/components/Projects.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import ProjectCard from '../builders/ProjectCards';
+import './Projects.css';
 
-export default function Projects(){
+export default function Projects() {
+    const projectsData = [
+        {
+            title: 'JamSphere',
+            description: 'Music sharing social media platform',
+            imageUrl: '/jamsphere.png', // You should place your project images in the public directory
+            liveLink: 'https://jamsphere-ad4807320222.herokuapp.com/',
+            githubLink: 'https://github.com/ashleytrollinger/JamSphere',
+        },
+        {
+            title: 'RemembrWhn',
+            description: 'Virtual time capsule to store memories for a designated period of time',
+            imageUrl: '/RemembrWhn.png', // You should place your project images in the public directory
+            liveLink: 'https://remembrwhn-nur09010i-ashleytrollinger.vercel.app/',
+            githubLink: 'https://github.com/ashleytrollinger/remembrwhn',
+        },
+        {
+            title: 'React Portfolio',
+            description: 'First time using react, first draft of my professional portfolio',
+            imageUrl: '/ReactP.png', // You should place your project images in the public directory
+            liveLink: 'https://ashleytrollinger.github.io/react-portfolio/',
+            githubLink: 'https://github.com/ashleytrollinger/react-portfolio',
+        },
+        {
+            title: 'Weather Dashboard',
+            description: 'Works with OpenWeatherMap to create a 7 day weather forcaster',
+            imageUrl: '/weather.png', // You should place your project images in the public directory
+            liveLink: '',
+            githubLink: 'https://github.com/ashleytrollinger/Weather-Dashboard',
+        },
+        
+        // Repeat this structure for your other projects
+    ];
     return (
-        <p>Projects</p>
+
+        <div className="projects-container">
+            <h2>Projects</h2>
+            {projectsData.map((project, index) => (
+                <ProjectCard key={index} {...project} />
+            ))}
+        </div>
     )
 }
